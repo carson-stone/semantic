5d193a9a2d717aa7edb7eeb70760f85919601757
Carson Stone <carson@landis.com>
2023-01-22 17:07:30 -0500

    add useful middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import Express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = Express();
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('hi');
 });
 
-app.listen('3000', () => {
-  console.log('running');
+app.listen(process.env.PORT, () => {
+  console.log(`Running on port ${process.env.PORT}`);
 });
